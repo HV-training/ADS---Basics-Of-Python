0b737a3888686181b68e9ea7fd548c67584991d3
HV-training <63405047+HV-training@users.noreply.github.com>
2020-04-17 13:54:29 +0100

    Minor update

--- 06_tuple.py
+++ 06_tuple.py
@@ -2,7 +2,7 @@
 
 """
 Execute the below code in a cell by cell and see the output. There are Assignments to do it by your own, so complete that as well
-Use the Spyder to run and execute these .py files
+Use Spyder to run and execute these .py files
 """
 
 
